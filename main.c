a64a377c8a946d7b43344a1dc58294edfa805f8a
Marko Tasic <mtasic85@gmail.com>
2019-06-12 20:52:29 +0200

    R_Object

--- main.c
+++ main.c
@@ -4,29 +4,31 @@
 #include <stdbool.h>
 #include <threads.h>
 
-enum _R_Type;
-union _R_Value;
-struct _R_Struct;
-struct _R_Union;
-struct _R_Enum;
+enum R_Type;
+union R_Value;
+struct R_Object;
+struct R_Struct;
+struct R_Union;
+struct R_Enum;
 
-typedef enum _R_Type {
-    _R_TYPE_B,
-    _R_TYPE_I8,
-    _R_TYPE_U8,
-    _R_TYPE_I16,
-    _R_TYPE_U16,
-    _R_TYPE_I32,
-    _R_TYPE_U32,
-    _R_TYPE_I64,
-    _R_TYPE_U64,
-    _R_TYPE_F32,
-    _R_TYPE_F64,
-    _R_TYPE_C,
-    _R_TYPE_P
-} _R_Type;
+typedef enum R_Type {
+    R_TYPE_B,
+    R_TYPE_I8,
+    R_TYPE_U8,
+    R_TYPE_I16,
+    R_TYPE_U16,
+    R_TYPE_I32,
+    R_TYPE_U32,
+    R_TYPE_I64,
+    R_TYPE_U64,
+    R_TYPE_F32,
+    R_TYPE_F64,
+    R_TYPE_C,
+    R_TYPE_P,
+    R_TYPE_STRUCT
+} R_Type;
 
-typedef union _R_Value {
+typedef union R_Value {
     bool b;
     int8_t i8;
     uint8_t u8;
@@ -40,79 +42,57 @@ typedef union _R_Value {
     double f64;
     char *c;
     void *p;
-} _R_Value;
+} R_Value;
 
-typedef struct _R_Object {
-    _R_Type type;
-    _R_Value value;
-} _R_Object;
+typedef struct R_Object {
+    uint64_t rc;
+    R_Type type;
+    R_Value value;
+    // Own, Borrow, Mut Borrow
+} R_Object;
 
-typedef struct _R_Struct {
-    char *name;
-    char **attrs;
-    int *sizes;
-} _R_Struct;
+typedef struct R_Struct {
+    // char *name;
+    // char **attrs;
+    // int *sizes;
 
-typedef struct _R_Union {
+} R_Struct;
 
-} _R_Union;
+typedef struct R_Union {
 
-typedef struct _R_Enum {
+} R_Union;
 
-} _R_Enum;
+typedef struct R_Enum {
+
+} R_Enum;
 
 /*
- * _RResult
+ * RResult
  */
-typedef enum _R_ResultType {
-    _R_OK,
-    _R_OK_I8,
-    _R_OK_U8,
-    _R_OK_I16,
-    _R_OK_U16,
-    _R_OK_I32,
-    _R_OK_U32,
-    _R_OK_I64,
-    _R_OK_U64,
-    _R_OK_F32,
-    _R_OK_F64,
-    _R_ERR
-} _R_ResultType;
+typedef enum R_ResultType {
+    R_RESULT_TYPE_OK,
+    R_RESULT_TYPE_ERR
+} R_ResultType;
 
-typedef union _R_ResultHolder {
-    void* ok;
-    int8_t i8;
-    uint8_t u8;
-    int16_t i16;
-    uint16_t u16;
-    int32_t i32;
-    uint32_t u32;
-    int64_t i64;
-    uint64_t u64;
-    float f32;
-    double f64;
-    char * err;
-} _R_ResultHolder;
-
-typedef struct _R_Result {
-    _R_ResultType type;
-    _R_ResultHolder value;
-} _R_Result;
+typedef struct R_Result {
+    R_ResultType type;
+    R_Object value;
+} R_Result;
 
 /*
  * _RArray
  */
-typedef struct _R_Array {
+/*typedef struct _R_Array {
     uint64_t cap;
     uint64_t len;
     uint64_t item_size;
     char * data;
-} _R_Array;
+} _R_Array;*/
 
 /*
  * _R_Array
  */
-_R_Result _R_Array_new(uint64_t cap, uint64_t len, uint64_t item_size);
+/*_R_Result _R_Array_new(uint64_t cap, uint64_t len, uint64_t item_size);
 _R_Result _R_Array_free(_R_Array * self);
 _R_Result _R_Array_append(_R_Array * self, void * item);
 _R_Result _R_Array_prepend(_R_Array * self, void * item);
@@ -186,13 +166,32 @@ void * _R_try(_R_Result r) {
     }
 
     return v;
+}*/
+
+void test_i8(void) {
+    R_Object i8_0 = {
+        .type = R_TYPE_I8,
+        .value = {.i8 = 10}
+    };
+
+    R_Object i8_1 = {
+        .type = R_TYPE_I8,
+        .value = {.i8 = 20}
+    };
+
+    R_Object i8_2 = {
+        .type = R_TYPE_I8,
+        .value = {.i8 = i8_0.value.i8 + i8_1.value.i8}
+    };
+
+    printf("i8_2=%d\n", i8_2.value.i8);
 }
 
 int main(int argc, char * argv[]) {
     /*
      * r0
      */
-    _R_Result r0 = _R_Array_new(8, 0, sizeof(int));
+    /*_R_Result r0 = _R_Array_new(8, 0, sizeof(int));
     _R_Array * a0 = NULL;
 
     switch (r0.type) {
@@ -207,12 +206,59 @@ int main(int argc, char * argv[]) {
             printf("error[%d]: supports only _R_OK and _R_ERR\n", __LINE__);
     }
 
-    _R_Array_free(a0);
+    _R_Array_free(a0);*/
 
     /*
      * r1
      */
-    _R_Array * a1 = _R_try(_R_Array_new(8, 0, sizeof(int)));
-    _R_Array_free(a1);
+    /*_R_Array * a1 = _R_try(_R_Array_new(8, 0, sizeof(int)));
+    _R_Array_free(a1);*/
+
+    test_i8();
+
+    R_Object i8_0 = {
+        .type = R_TYPE_I8,
+        .value = {.i8 = 10}
+    };
+
+    R_Object i8_1 = {
+        .type = R_TYPE_I8,
+        .value = {.i8 = 20}
+    };
+
+    R_Object _arr_0[2] = {i8_0, i8_1};
+
+    R_Object arr_0 = {
+        .type = R_TYPE_I8,
+        .value = {.p = &_arr_0}
+    };
+
+    R_Object struct_0 = {
+        .type = R_TYPE_STRUCT,
+        .value = {.c = "i8,i8"}
+    };
+
+    /**/
+    R_Object r_array_struct = {
+        .type = R_TYPE_STRUCT,
+        .value = {.c =
+            "len u64;"
+            "cap u64;"
+            "data p;"
+        }
+    };
+
+    /*
+    R_Object r_array_0 = {
+        .type: R_TYPE_STRUCT_INST,
+        .value = {.p = R_Object_new(
+            r_array_struct,
+            (R_Object){.type = R_TYPE_U64, .value = {.u64 = 0}},
+            (R_Object){.type = R_TYPE_U64, .value = {.u64 = 8}},
+            (R_Object){.type = R_TYPE_P, .value = {.p = malloc(8 * sizeof(R_Object))}},
+        )}
+    };
+    */
+
     return 0;
 }
